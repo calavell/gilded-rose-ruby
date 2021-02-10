c08c583a47039879cbecfcbf457bb0170f2ea53a
calavell <calavell@tcd.ie>
2021-02-10 11:11:33 +0000

    project setup & update_class method working for 'normal' conditions

---
+++ spec/shop_spec.rb
@@ -0,0 +1,39 @@
+require 'shop'
+require 'item'
+
+describe Shop do
+  describe "#update_quality" do
+    context 'inventory 1 normal item' do
+      let(:inventory) { [Item.new("toy", 20, 10)] }
+      let(:shop) { described_class.new(inventory) }
+      it "does not change the name of the items" do
+        shop.update_quality
+        expect(shop.items[0].name).to eq "toy"
+      end
+
+      it "decreases sellin by 1 for all items" do
+        shop.update_quality
+        expect(shop.items[0].sell_in).to eq 19
+      end
+
+      it "decreases quality by 1 for all items" do
+        shop.update_quality
+        expect(shop.items[0].quality).to eq 9
+      end
+    end
+
+    context 'normal item with min quality' do
+      let(:inventory) { [Item.new("toy", 20, 0)] }
+      let(:shop) { described_class.new(inventory) }
+      it 'does not decrease quality past zero' do
+        shop.update_quality
+        expect(shop.items[0].quality).to eq 0
+      end
+
+      it 'does decrease sellin still' do
+        shop.update_quality
+        expect(shop.items[0].sell_in).to eq 19
+      end
+    end
+  end
+end
